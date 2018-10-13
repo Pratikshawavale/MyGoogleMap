841755b2cb0056b9aa380f95f1595840f518f006
Pratikshawavale <41820589+Pratikshawavale@users.noreply.github.com>
2018-10-13 11:13:59 +0530

    first commit

--- app/src/main/java/com/example/pratiksha/mygooglemap/MapsActivity.java
+++ app/src/main/java/com/example/pratiksha/mygooglemap/MapsActivity.java
@@ -39,7 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(
+                        R.id.map);
         mapFragment.getMapAsync(this);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
